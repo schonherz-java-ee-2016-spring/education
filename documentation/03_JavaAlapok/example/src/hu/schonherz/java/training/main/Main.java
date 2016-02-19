86a7cba7330a252cba6a8ff470c4abc8198f70aa
Laszlo Farkas <laszlo.farkas@nebu.local>
2016-02-19 20:14:40 +0100

    Java alapok - classpath fix

--- documentation/03_JavaAlapok/example/src/hu/schonherz/java/training/main/Main.java
+++ documentation/03_JavaAlapok/example/src/hu/schonherz/java/training/main/Main.java
@@ -3,6 +3,10 @@ package hu.schonherz.java.training.main;
 public class Main {
 
   public static void main(String[] args) {
+    test();
+  }
+  
+  private static void test() {
     System.out.println("kacsa");
   }
 
